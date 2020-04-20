f82bf936f00433e29012a21fc3c75f3bba58079c
Vikram Singh <vikramverma1997@gmail.com>
2020-04-20 19:22:17 +0530

    checkboard program

--- Assignment1/Assignment1/CheckerboardKarel.py
+++ Assignment1/Assignment1/CheckerboardKarel.py
@@ -11,14 +11,67 @@ sample worlds supplied in the starter folder.
 
 
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    pass
+    place_all_beepers()
 
+
+def place_all_beepers():
+    while front_is_clear():
+        place_one_row()
+
+
+def place_one_row():
+    while front_is_clear():
+        put_beeper()
+        move()
+        if front_is_clear():
+            move()
+            if front_is_blocked():
+                put_beeper()
+    # return_back()
+    check_beeper()
+
+
+"""
+def return_back():
+   turn_around()
+   while front_is_clear():
+     move()
+   check_beeper()
+"""
+
+def check_beeper():
+    if beepers_present():
+        re_align()
+        if front_is_clear():
+            move()
+    else:
+        re_align()
+
+
+def re_align():
+    if facing_east():
+        turn_left()
+        if front_is_clear():
+            move()
+            turn_left()
+    else:
+        turn_right()
+        if front_is_clear():
+            move()
+            turn_right()
+
+
+"""
+def turn_around():
+    turn_left()
+    turn_left()
+"""
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
 # There is no need to edit code beyond this point
 
