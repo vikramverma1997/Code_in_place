b0538e17ab81c526e0320da8d56b2b3be60ab009
Vikram Singh <vikramverma1997@gmail.com>
2020-04-28 00:37:56 +0530

    hailstones program

--- Assignment2/Assignment2/hailstones.py
+++ Assignment2/Assignment2/hailstones.py
@@ -4,9 +4,44 @@ File: hailstones.py
 This is a file for the optional Hailstones problem, if
 you'd like to try solving it.
 """
-import numpy as np
+
+STOP_NUMBER = 1
+
+
+def check_num(num):
+    remainder = num % 2
+    if remainder == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+
+    return even_or_odd
+
 
 def main():
+    count = 0
+    num = int(input("Enter a positive integer: "))
+    while num < 0:
+        num = int(input("Please enter a positive integer: "))
+
+    while num != STOP_NUMBER:
+        even_or_odd = check_num(num)
+        if even_or_odd == "even":
+            num1 = int(num / 2)
+            print(str(num) + " is even, so I take half: " + str(num1))
+        else:
+            num1 = int((3 * num) + 1)
+            print(str(num) + " is odd, so I take 3n + 1: " + str(num1))
+        num = num1
+        count += 1
+
+    print("The process took " + str(count) + " steps to reach 1")
+
+
+
+
+
+
 
 
 # This provided line is required at the end of a Python file
