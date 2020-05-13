c22a958730614c21eaac498bca00c60f9972e386
Vikram Singh <vikramverma1997@gmail.com>
2020-05-13 23:28:12 +0530

    Add english to pig latin

---
+++ Assignments/Worked_examples/pig_latin_translator.py
@@ -0,0 +1,15 @@
+"""
+Filename: pig_latin_translator
+------------------------------
+This program translates English language into Pig Latin language
+"""
+
+
+def main():
+    print('This program translates words/phrases from English to Pig Latin. To end the program. type EXIT')
+    user_input = input('Word/phrase to translate: ')
+
+
+
+if __name__ == '__main__':
+    main()
